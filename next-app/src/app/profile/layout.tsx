2789bba00c8a141611e7537a126ddb89df646d9b
Jonatt-001 <1jonatttech@gmail.com>
2025-12-12 16:36:04 +0100

    Update layout.tsx

--- next-app/src/app/profile/layout.tsx
+++ next-app/src/app/profile/layout.tsx
@@ -2,8 +2,8 @@ import { Metadata } from 'next';
 import React from 'react'
 
 export const metadata: Metadata = {
-    title: 'CodeFramer | User Profile - View & Customize Your Developer Profile',
-    description: "Access your CodeFramer user profile to update personal info, manage account settings, and view your coding activity across projects.",
+    title: 'Kaali Coder | User Profile - View & Customize Your Developer Profile',
+    description: "Access your Kaali Coder user profile to update personal information, manage account settings, and track your activity across projects.",
     robots: {
         index: false,
         follow: false
